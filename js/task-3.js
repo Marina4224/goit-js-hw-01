9b813285ccf9f9dcfdf61000fd136d9f25939406
Marina <marinka1984mmm@gmail.com>
2025-04-05 17:47:26 +0300

    add dz three

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,9 @@
 function getElementWidth(content, padding, border) {
-    content = Number.parseFloat(content);
-    padding = Number.parseFloat(padding);
-    border = Number.parseFloat(border);
-    return getElementWidth = content + 2 * padding + 2 * border;
+   const contentWidth = Number.parseFloat(content);
+    const paddingWidth = Number.parseFloat(padding);
+   const borderWidth = Number.parseFloat(border);
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+    return totalWidth
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
